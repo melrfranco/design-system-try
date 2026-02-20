d916a562efa1386d25b5e6b46c75441811f4f150
melrfranco <mrfranco@asu.edu>
2026-02-19 16:59:12 -0800

    Update App.tsx

--- design-system-editor-951/client/App.tsx
+++ design-system-editor-951/client/App.tsx
@@ -1,18 +1,18 @@
 import "./global.css";
 
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
+<HashRouter>
+  <Routes>
       <Route path="/" element={<Index />} />
       {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
       <Route path="*" element={<NotFound />} />
     </Routes>
-  </BrowserRouter>
+</HashRouter>
 );
 
 createRoot(document.getElementById("root")!).render(<App />);
